069661d5706acd399b9f490251dc177c270d7a33
Mike Sperber <sperber@deinprogramm.de>
2020-01-28 13:09:25 +0100

    Various updates for APICon blog post

--- properties/ShoppingCart.fs
+++ properties/ShoppingCart.fs
@@ -13,6 +13,11 @@ module Price =
   let add (Price amount1) (Price amount2) = Price (amount1 + amount2)
   let scale factor (Price amount) = Price (factor * amount)
 
+type Catalog = Map<Item, Price>
+
+module Catalog =
+  let itemPrice (catalog: Catalog) (item: Item): Price =
+    Map.find item catalog
 
 type Interface =
 
@@ -22,206 +27,252 @@ type Interface =
 
   abstract member TotalCount: unit -> int
 
+type Discount = { receive: int; payFor: int }
+
+type Discounts = Map<Item, Discount>
+
+module Discounts =
+    let empty = Map.empty
+
+type Factory =
+    abstract member make: Catalog -> Interface
+
+    abstract member make: Catalog * Discounts -> Interface
+
 module Interface =
   let add (cart: Interface) (count: int) (item: Item) = cart.Add count item
   let total (cart: Interface): Price = cart.Total ()
   let totalCount (cart: Interface): int = cart.TotalCount ()
 
-type Discount = { receive: int; payFor: int }
 
 module Implementation =
  // caught out by totalCountCorrect
- let create1 (prices: Map<Item, Price>) =
-   let cart = ref Map.empty<Item, int>
-   { new Interface with
-      member __.Add count item =
-        cart := Map.add item count !cart
-      member __.Total () =
-        let folder total item count =
-          Price.add total (Price.scale count (Map.find item prices))
-        Map.fold folder Price.nothing !cart 
+ let factory1 =
+    { new Factory with
+       member __.make(catalog) =
+           let cart = ref Map.empty<Item, int>
+           { new Interface with
+              member __.Add count item =
+                cart := Map.add item count !cart
+              member __.Total () =
+                let folder total item count =
+                  Price.add total (Price.scale count (Map.find item catalog))
+                Map.fold folder Price.nothing !cart 
 
-      member __.TotalCount () =
-         let folder total item count = total + count
-         Map.fold folder 0 !cart
-   }
+              member __.TotalCount () =
+                 let folder total item count = total + count
+                 Map.fold folder 0 !cart
+           }
+       member __.make(catalog, discounts) = failwith "unimplemented"
+    }
 
  // ignores count, caught out by itemCountCorrect
- let create2 (prices: Map<Item, Price>) =
-   let cart = ref Map.empty<Item, int>
-   let itemCount item =
-       match Map.tryFind item !cart with
-       | None -> 0
-       | Some count -> count
-   { new Interface with
-      member __.Add count item =
-        cart := Map.add item (itemCount item + 1) !cart
+ let factory2 =
+    { new Factory with
+       member __.make(catalog) =
+         let cart = ref Map.empty<Item, int>
+         let itemCount item =
+             match Map.tryFind item !cart with
+             | None -> 0
+             | Some count -> count
+         { new Interface with
+            member __.Add count item =
+              cart := Map.add item (itemCount item + 1) !cart
 
-      member __.Total () =
-        let folder total item count =
-          Price.add total (Price.scale count (Map.find item prices))
-        Map.fold folder Price.nothing !cart 
+            member __.Total () =
+              let folder total item count =
+                Price.add total (Price.scale count (Map.find item catalog))
+              Map.fold folder Price.nothing !cart 
 
-      member __.TotalCount () =
-         let folder total item count = total + count
-         Map.fold folder 0 !cart
-   }
+            member __.TotalCount () =
+               let folder total item count = total + count
+               Map.fold folder 0 !cart
+         }
+       member __.make(catalog, discounts) = failwith "unimplemented"
+    }
 
  // correct wrt naive model
- let create3 (prices: Map<Item, Price>) =
-   let cart = ref Map.empty<Item, int>
-   let itemCount item =
-       match Map.tryFind item !cart with
-       | None -> 0
-       | Some count -> count
-   { new Interface with
-      member __.Add count item =
-        cart := Map.add item (itemCount item + count) !cart
+ let factory3 =
+    { new Factory with
+       member __.make(catalog) =
+         let cart = ref Map.empty<Item, int>
+         let itemCount item =
+             match Map.tryFind item !cart with
+             | None -> 0
+             | Some count -> count
+         { new Interface with
+            member __.Add count item =
+              cart := Map.add item (itemCount item + count) !cart
 
-      member __.Total () =
-        let folder total item count =
-          Price.add total (Price.scale count (Map.find item prices))
-        Map.fold folder Price.nothing !cart 
+            member __.Total () =
+              let folder total item count =
+                Price.add total (Price.scale count (Map.find item catalog))
+              Map.fold folder Price.nothing !cart 
 
-      member __.TotalCount () =
-         let folder total item count = total + count
-         Map.fold folder 0 !cart
-   }
+            member __.TotalCount () =
+               let folder total item count = total + count
+               Map.fold folder 0 !cart
+         }
+       member __.make(catalog, discounts) = failwith "unimplemented"
+    }
 
  // truncates item names, caught out by onePriceCorrect
- let create4 (prices': Map<Item, Price>) =
-   let trunc (Item itemName, price) = (Item (itemName.Substring (0, min 12 (String.length itemName))), price)
-   let prices = prices' |> Map.toSeq |> Seq.map trunc |> Map.ofSeq
-   let cart = ref Map.empty<Item, int>
-   let itemCount item =
-       match Map.tryFind item !cart with
-       | None -> 0
-       | Some count -> count
-   { new Interface with
-      member __.Add count item =
-        cart := Map.add item (itemCount item + count) !cart
+ let factory4 =
+    { new Factory with
+       member __.make(catalog) =
+         let trunc (Item itemName, price) = (Item (itemName.Substring (0, min 12 (String.length itemName))), price)
+         let prices = catalog |> Map.toSeq |> Seq.map trunc |> Map.ofSeq
+         let cart = ref Map.empty<Item, int>
+         let itemCount item =
+             match Map.tryFind item !cart with
+             | None -> 0
+             | Some count -> count
+         { new Interface with
+            member __.Add count item =
+              cart := Map.add item (itemCount item + count) !cart
 
-      member __.Total () =
-        let folder total item count =
-          Price.add total (Price.scale count (Map.find item prices))
-        Map.fold folder Price.nothing !cart 
+            member __.Total () =
+              let folder total item count =
+                Price.add total (Price.scale count (Map.find item prices))
+              Map.fold folder Price.nothing !cart 
 
-      member __.TotalCount () =
-         let folder total item count = total + count
-         Map.fold folder 0 !cart
-   }
+            member __.TotalCount () =
+               let folder total item count = total + count
+               Map.fold folder 0 !cart
+         }
+       member __.make(catalog, discounts) = failwith "unimplemented"
+    }
 
  // no negative counts; catches out int in itemCountCorrect: need a model!
- let create5 (prices: Map<Item, Price>) =
-   let cart = ref Map.empty<Item, int>
-   let itemCount item =
-       match Map.tryFind item !cart with
-       | None -> 0
-       | Some count -> count
-   { new Interface with
-      member __.Add count item =
-        cart := Map.add item (max 0 (itemCount item + count)) !cart
+ let factory5 =
+    { new Factory with
+       member __.make(catalog) =
+         let cart = ref Map.empty<Item, int>
+         let itemCount item =
+             match Map.tryFind item !cart with
+             | None -> 0
+             | Some count -> count
+         { new Interface with
+            member __.Add count item =
+              cart := Map.add item (max 0 (itemCount item + count)) !cart
 
-      member __.Total () =
-        let folder total item count =
-          Price.add total (Price.scale count (Map.find item prices))
-        Map.fold folder Price.nothing !cart 
+            member __.Total () =
+              let folder total item count =
+                Price.add total (Price.scale count (Map.find item catalog))
+              Map.fold folder Price.nothing !cart 
 
-      member __.TotalCount () =
-         let folder total item count = total + count
-         Map.fold folder 0 !cart
-   }
+            member __.TotalCount () =
+               let folder total item count = total + count
+               Map.fold folder 0 !cart
+         }
+       member __.make(catalog, discounts) = failwith "unimplemented"
+    }
 
  // ignores count when there's a discount
- let create6 (prices: Map<Item, Price>) (discounts: Map<Item, Discount>) =
-   let cart = ref Map.empty<Item, int>
-   let itemCount item =
-       match Map.tryFind item !cart with
-       | None -> 0
-       | Some count -> count
-   let calculatePrice (price: Price) (count: int) (discount: Option<Discount>): Price =
-     match discount with
-     | None -> Price.scale count price
-     | Some { receive = receive; payFor = payFor } ->
-         if count >= receive
-         then Price.scale payFor price
-         else Price.scale count price
+ let factory6 =
+    { new Factory with
+       member factory.make(catalog) =
+         factory.make(catalog, Discounts.empty)
+       member __.make(catalog, discounts) =
+         let cart = ref Map.empty<Item, int>
+         let itemCount item =
+             match Map.tryFind item !cart with
+             | None -> 0
+             | Some count -> count
+         let calculatePrice (price: Price) (count: int) (discount: Option<Discount>): Price =
+           match discount with
+           | None -> Price.scale count price
+           | Some { receive = receive; payFor = payFor } ->
+               if count >= receive
+               then Price.scale payFor price
+               else Price.scale count price
 
-   { new Interface with
-      member __.Add count item =
-        cart := Map.add item (itemCount item + count) !cart
+         { new Interface with
+            member __.Add count item =
+              cart := Map.add item (itemCount item + count) !cart
 
-      member __.Total () =
-        let folder total item count =
-          let price = Map.find item prices
-          let optionalDiscount = Map.tryFind item discounts
-          Price.add total (calculatePrice price count optionalDiscount)
-        Map.fold folder Price.nothing !cart 
+            member __.Total () =
+              let folder total item count =
+                let price = Map.find item catalog
+                let optionalDiscount = Map.tryFind item discounts
+                Price.add total (calculatePrice price count optionalDiscount)
+              Map.fold folder Price.nothing !cart 
 
-      member __.TotalCount () =
-         let folder total item count = total + count
-         Map.fold folder 0 !cart
-   }
+            member __.TotalCount () =
+               let folder total item count = total + count
+               Map.fold folder 0 !cart
+         }
+    }
 
  // leaves out remainder when calculating discount
- let create7 (prices: Map<Item, Price>) (discounts: Map<Item, Discount>) =
-   let cart = ref Map.empty<Item, int>
-   let itemCount item =
-       match Map.tryFind item !cart with
-       | None -> 0
-       | Some count -> count
-   let calculatePrice (price: Price) (count: int) (discount: Option<Discount>): Price =
-     match discount with
-     | None -> Price.scale count price
-     | Some { receive = receive; payFor = payFor } ->
-         if count >= receive
-         then 
-            // BUG: remainder
-            Price.scale ((count / receive) * payFor) price
-         else Price.scale count price
-   { new Interface with
-      member __.Add count item =
-        cart := Map.add item (itemCount item + count) !cart
+ let factory7  =
+   { new Factory with
+       member factory.make(catalog) =
+         factory.make(catalog, Discounts.empty)
+       member __.make(catalog, discounts) =
+         let cart = ref Map.empty<Item, int>
+         let itemCount item =
+             match Map.tryFind item !cart with
+             | None -> 0
+             | Some count -> count
+         let calculatePrice (price: Price) (count: int) (discount: Option<Discount>): Price =
+           match discount with
+           | None -> Price.scale count price
+           | Some { receive = receive; payFor = payFor } ->
+               if count >= receive
+               then 
+                  // BUG: remainder
+                  Price.scale ((count / receive) * payFor) price
+               else Price.scale count price
+         { new Interface with
+            member __.Add count item =
+              cart := Map.add item (itemCount item + count) !cart
 
-      member __.Total () =
-        let folder total item count =
-          let price = Map.find item prices
-          let optionalDiscount = Map.tryFind item discounts
-          Price.add total (calculatePrice price count optionalDiscount)
-        Map.fold folder Price.nothing !cart 
+            member __.Total () =
+              let folder total item count =
+                let price = Map.find item catalog
+                let optionalDiscount = Map.tryFind item discounts
+                Price.add total (calculatePrice price count optionalDiscount)
+              Map.fold folder Price.nothing !cart 
 
-      member __.TotalCount () =
-         let folder total item count = total + count
-         Map.fold folder 0 !cart
+            member __.TotalCount () =
+               let folder total item count = total + count
+               Map.fold folder 0 !cart
+         }
    }
 
  // OK
- let create8 (prices: Map<Item, Price>) (discounts: Map<Item, Discount>) =
-   let cart = ref Map.empty<Item, int>
-   let itemCount item =
-       match Map.tryFind item !cart with
-       | None -> 0
-       | Some count -> count
-   let calculatePrice (price: Price) (count: int) (discount: Option<Discount>): Price =
-     match discount with
-     | None -> Price.scale count price
-     | Some { receive = receive; payFor = payFor } ->
-         if count >= receive
-         then 
-            Price.scale (((count / receive) * payFor) + count % receive) price
-         else Price.scale count price
-   { new Interface with
-      member __.Add count item =
-        cart := Map.add item (itemCount item + count) !cart
+ let factory8 =
+    { new Factory with
+       member factory.make(catalog) =
+         factory.make(catalog, Discounts.empty)
+       member __.make(catalog, discounts) =
+         let cart = ref Map.empty<Item, int>
+         let itemCount item =
+             match Map.tryFind item !cart with
+             | None -> 0
+             | Some count -> count
+         let calculatePrice (price: Price) (count: int) (discount: Option<Discount>): Price =
+           match discount with
+           | None -> Price.scale count price
+           | Some { receive = receive; payFor = payFor } ->
+               if count >= receive
+               then 
+                  Price.scale (((count / receive) * payFor) + count % receive) price
+               else Price.scale count price
+         { new Interface with
+            member __.Add count item =
+              cart := Map.add item (itemCount item + count) !cart
 
-      member __.Total () =
-        let folder total item count =
-          let price = Map.find item prices
-          let optionalDiscount = Map.tryFind item discounts
-          Price.add total (calculatePrice price count optionalDiscount)
-        Map.fold folder Price.nothing !cart 
+            member __.Total () =
+              let folder total item count =
+                let price = Map.find item catalog
+                let optionalDiscount = Map.tryFind item discounts
+                Price.add total (calculatePrice price count optionalDiscount)
+              Map.fold folder Price.nothing !cart 
 
-      member __.TotalCount () =
-         let folder total item count = total + count
-         Map.fold folder 0 !cart
-   }
+            member __.TotalCount () =
+               let folder total item count = total + count
+               Map.fold folder 0 !cart
+         }
+    }
--- properties/ShoppingCartProperties.fs
+++ properties/ShoppingCartProperties.fs
@@ -9,38 +9,38 @@ module Test =
     let item = Arb.convert Item Item.itemName Arb.letterString
     let mapIsNonEmpty map = not (Map.isEmpty map)
 
-    let prices = Arb.filter mapIsNonEmpty (Arb.map item price)
+    let catalog = Arb.filter mapIsNonEmpty (Arb.map item price)
 
     // First version: gets caught out by addingItemIncreasesTotal
     let discount' =
-      let arbN = Arb.choose 1 20
+      let arbCount = Arb.choose 1 20
       let convertTo (receive, payFor) = { receive = receive; payFor = payFor }
       let convertFrom discount = (discount.receive, discount.payFor)
       let valid (receive, payFor) = payFor < receive
-      Arb.convert convertTo convertFrom (Arb.filter valid (Arb.pair arbN arbN))
+      Arb.convert convertTo convertFrom (Arb.filter valid (Arb.pair arbCount arbCount))
 
     let discount =
       let convertTo receive = { receive = receive; payFor = receive-1 }
       let convertFrom discount = discount.receive
       Arb.convert convertTo convertFrom (Arb.choose 2 20)
 
-    let discounts prices =
-      let items = Seq.map fst (Map.toSeq prices)
+    let discounts catalog =
+      let items = Seq.map fst (Map.toSeq catalog)
       Arb.map (Arb.pickOneOf items) discount
 
-    let priceAndDiscounts =
+    let catalogAndDIscounts =
       let gen = gen {
-           let! prices = Arb.toGen prices
-           let! discounts = Arb.toGen (discounts prices)
-           return (prices, discounts)
+           let! catalog = Arb.toGen catalog
+           let! discounts = Arb.toGen (discounts catalog)
+           return (catalog, discounts)
           }
       in Arb.fromGen gen
 
-    let countAndItem prices =
-      let items = Seq.map fst (Map.toSeq prices)
+    let countAndItem catalog =
+      let items = Seq.map fst (Map.toSeq catalog)
       Arb.pair (Arb.choose 1 50) (Arb.pickOneOf items) // int before 
 
-    let countAndItems prices = Arb.list (countAndItem prices)
+    let countAndItems catalog = Arb.list (countAndItem catalog)
 
   type Arbs =
     static member Price () = Arb.price
@@ -52,53 +52,87 @@ module Test =
   let (.>=.) left right = left >= right |@ sprintf "%A >= %A" left right
   let (.>.) left right = left > right |@ sprintf "%A > %A" left right
 
-  let total0Correct (create: Map<Item, Price> -> Interface) =
-    Prop.forAll Arb.prices (fun prices ->
-      let cart = create prices
-      Interface.totalCount cart .=. 0)
+  let assertEquals (a: 'T when 'T: equality) (b: 'T) =
+    if a <> b
+    then failwith "test failed"
+    else ()
+
+  let test (factory: Factory) =
+    let cart1 = factory.make Map.empty
+    assertEquals (Interface.total cart1) (Price 0)
+    let cart2 = factory.make (Map.ofList [Item "Milk", Price 100; Item "Egg", Price 20])
+    assertEquals (Interface.total cart2) (Price 0)
+  
+  let total0Correct (factory: Factory) =
+    Prop.forAll Arb.catalog (fun catalog ->
+      let cart = factory.make catalog
+      printfn "%A" catalog
+      Interface.total cart .=. Price 0)
    
   let totalCountCorrect create =
-    Prop.forAll Arb.prices (fun prices ->
+    Prop.forAll Arb.catalog (fun prices ->
       let items = List.map fst (Map.toList prices)
       Prop.forAll (Arb.list (Arb.pickOneOf items)) (fun items ->
         let cart = create prices
         List.iter (Interface.add cart 1) items
         cart.TotalCount () .=. List.length items))
 
+  let totalCorrect (factory: Factory) =
+    Prop.forAll Arb.catalog (fun catalog ->
+      let items = List.map fst (Map.toList catalog)
+      Prop.forAll (Arb.list (Arb.pickOneOf items)) (fun items ->
+        let cart = factory.make catalog
+        List.iter (Interface.add cart 1) items
+        let prices = List.map (Catalog.itemPrice catalog) items
+        let total = List.fold Price.add (Price 0) prices
+        Interface.total cart .=. total))
+
+  let totalNCorrect (factory: Factory) =
+    Prop.forAll Arb.catalog (fun catalog ->
+      let items = List.map fst (Map.toList catalog)
+      Prop.forAll (Arb.list (Arb.pair Arb.positiveInt (Arb.pickOneOf items))) (fun countsItems ->
+          let cart = factory.make catalog
+          List.iter (fun (count, item) -> Interface.add cart count item) countsItems
+          let countItemPrice (count, item) =
+                Price.scale count (Catalog.itemPrice catalog item)
+          let prices = List.map countItemPrice countsItems
+          let total = List.fold Price.add (Price 0) prices
+          Interface.total cart .=. total))
+
   // doesn't know about counts going below zero
-  let itemCountCorrect create =
-    Prop.forAll Arb.prices (fun prices ->
-      let item = fst (Seq.head (Map.toSeq prices))
+  let itemCountCorrect (factory: Factory) =
+    Prop.forAll Arb.catalog (fun catalog ->
+      let item = fst (Seq.head (Map.toSeq catalog))
       Prop.forAll (Arb.list Arb.int) (fun counts ->
-        let cart = create prices
+        let cart = factory.make catalog
         List.iter (fun count -> Interface.add cart count item) counts
         Interface.totalCount cart .=. List.sum counts))
-        
-  let onePriceCorrect create =
-    Prop.forAll Arb.prices (fun prices ->
-      let itemPrice = Seq.head (Map.toSeq prices)
-      let cart = create prices
+
+  let onePriceCorrect (factory: Factory) =
+    Prop.forAll Arb.catalog (fun catalog ->
+      let itemPrice = Seq.head (Map.toSeq catalog)
+      let cart = factory.make catalog
       Interface.add cart 1 (fst itemPrice)
       Interface.total cart .=. snd itemPrice)
-      
-  let addingItemIncreasesTotal (create: Map<Item, Price> -> Map<Item, Discount> -> Interface) =
-    Prop.forAll Arb.prices (fun prices ->
-      Prop.forAll (Arb.discounts prices) (fun discounts ->
-        Prop.forAll (Arb.countAndItems prices) (fun countAndItems ->
-          let cart = create prices discounts
+
+  let addingItemIncreasesTotal (factory: Factory) =
+    Prop.forAll Arb.catalog (fun catalog ->
+      Prop.forAll (Arb.discounts catalog) (fun discounts ->
+        Prop.forAll (Arb.countAndItems catalog) (fun countAndItems ->
+          let cart = factory.make(catalog, discounts)
           List.iter (fun (count, item) -> Interface.add cart count item) countAndItems
-          let itemPrice = Seq.head (Map.toSeq prices)
+          let itemPrice = Seq.head (Map.toSeq catalog)
           let totalBefore = Interface.total cart
-          Interface.add cart 2 (fst itemPrice)  // start with 1
+          Interface.add cart 1 (fst itemPrice)  // start with 1
           let totalAfter = Interface.total cart
-          totalAfter .>. totalBefore))) // 2 for 1 might keep price the same
+          totalAfter .>=. totalBefore))) // 2 for 1 might keep price the same
 
   // model-based testing
   open FsCheck.Experimental
 
   type Model = {
-    prices: Map<Item, Price>
-    discounts: Map<Item, Discount>
+    catalog: Catalog
+    discounts: Discounts
     cart: Map<Item, int>
   }
 
@@ -119,9 +153,9 @@ module Test =
         then Price.scale (((count / receive) * payFor) + count % receive) price
         else Price.scale count price
 
-  let total { prices = prices; discounts = discounts; cart = cart } =
+  let total { catalog = catalog; discounts = discounts; cart = cart } =
     let folder total item count =
-      let price = Map.find item prices
+      let price = Map.find item catalog
       let optionalDiscount = Map.tryFind item discounts
       Price.add total (calculatePrice price count optionalDiscount)
     Map.fold folder Price.nothing cart 
@@ -129,7 +163,7 @@ module Test =
   let totalCount model =
     Seq.sum (Seq.map snd (Map.toSeq model.cart))
 
-  let cartMachine (create: Map<Item, Price> -> Map<Item, Discount> -> Interface) =
+  let cartMachine (factory: Factory) =
     let add (count, item) =
       { new Operation<Interface, Model>() with
           override __.Run model =
@@ -141,21 +175,21 @@ module Test =
               ((Interface.total sut .=. total model) %> "total")
           override __.ToString () = sprintf "add %A %A" count item
       }
-    let create (prices, discounts) =
+    let create (catalog, discounts) =
       { new Setup<Interface, Model>() with
-          override __.Actual () = create prices discounts
-          override __.Model () = { prices = prices; discounts = discounts; cart = Map.empty }
+          override __.Actual () = factory.make(catalog, discounts)
+          override __.Model () = { catalog = catalog; discounts = discounts; cart = Map.empty }
       }
     { new Machine<Interface, Model> () with
         override __.Setup =
           let convertFrom (setup: Setup<Interface, Model>) = 
             let model = setup.Model ()
-            (model.prices, model.discounts)
-          let convertTo (prices, discounts) = create (prices, discounts)
-          Arb.convert convertTo convertFrom Arb.priceAndDiscounts
+            (model.catalog, model.discounts)
+          let convertTo (catalog, discounts) = create(catalog, discounts)
+          Arb.convert convertTo convertFrom Arb.catalogAndDIscounts
         override __.Next model =
-          Gen.map add (Arb.toGen (Arb.countAndItem model.prices))
-          
+          Gen.map add (Arb.toGen (Arb.countAndItem model.catalog))
+
     }
 
 
